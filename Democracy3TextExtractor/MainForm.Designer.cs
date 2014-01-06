f3b7b733544205e2b6bf8dc277972378e1907707
Dragouf <da.laurent@laposte.net>
2014-01-06 17:05:48 +0800

    - Add instructions to ReadMe file

--- Democracy3TextExtractor/MainForm.Designer.cs
+++ Democracy3TextExtractor/MainForm.Designer.cs
@@ -40,13 +40,13 @@
             this.openFileDialogTransifex = new System.Windows.Forms.OpenFileDialog();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.btExtract = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxOutputFolder = new System.Windows.Forms.TextBox();
             this.btOutput = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -111,9 +111,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(7, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.Size = new System.Drawing.Size(127, 14);
             this.label2.TabIndex = 8;
             this.label2.Text = "Transifex translation file";
             // 
@@ -148,19 +149,25 @@
             this.tabPage1.Text = "Extraction";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // label5
             // 
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.btCompile);
-            this.tabPage2.Controls.Add(this.textBoxTransifexFile);
-            this.tabPage2.Controls.Add(this.btTransifexFile);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(804, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Injection";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(139, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "democracy3ExtractedText.ini";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 14);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Filename";
             // 
             // btExtract
             // 
@@ -200,25 +207,19 @@
             this.btOutput.UseVisualStyleBackColor = true;
             this.btOutput.Click += new System.EventHandler(this.btOutput_Click);
             // 
-            // label4
+            // tabPage2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(7, 52);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 14);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Filename";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(139, 52);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(141, 13);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "democracy3ExtractedText.ini";
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.btCompile);
+            this.tabPage2.Controls.Add(this.textBoxTransifexFile);
+            this.tabPage2.Controls.Add(this.btTransifexFile);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(804, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Injection";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 Democracy3TextExtractor
 =======================
 
-A tool to extract language strings from Democracy 3 and reinject them after translation through transifex website
+## Usage
+
+1. Download transifex translated file
+
+Go to https://www.transifex.com/projects/p/democracy-3/ then select your language, click on d3translation.ini and click on "Download fir use"
+
+<p align="center">
+<img src="https://raw.github.com/dragouf/Democracy3TextExtractor/master/doc/info1.PNG" alt="1 step screenshot" />
+</p>
+
+2. Download program release
+
+<p align="center">
+<img src="https://raw.github.com/dragouf/Democracy3TextExtractor/master/doc/info2.PNG" alt="2 step screenshot" />
+</p>
+
+
+<p align="center">
+<img src="https://raw.github.com/dragouf/Democracy3TextExtractor/master/doc/info3.PNG" alt="3 step screenshot" />
+</p>
+
+3. Unzip and execute Democracy3TextExtractor.exe
+
+4. Open "Injection" tab and click on "..." then select previous file you download on transifex and click on "Re-inject Text"
+
+<p align="center">
+<img src="https://raw.github.com/dragouf/Democracy3TextExtractor/master/doc/info4.PNG" alt="4 step screenshot" />
+</p>
+
+And voila, game should be translate with last translation file.
+
+Enjoy!
+
+
